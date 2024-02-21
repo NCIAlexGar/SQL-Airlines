0585c980c0a52ac9bf65dfbd5d06042340a291e8
Alex <116267528+NCIAlexGar@users.noreply.github.com>
2024-02-21 21:19:04 +0000

    Add files via upload

---
+++ seat.sql
@@ -0,0 +1,6 @@
+SELECT * FROM airline.seat;INSERT INTO seat(seat_id, aircraft_code, seat_number, seat_option) VALUES(00852,"A319","ST17B","economic"), (00456,"A321","ST21C","economic"), (00753,"A320","ST05B","economic"), (00357,"A321","ST10A","economic"), (00159,"A319","ST33C","economic"),
+ (00147,"A320","ST25A","economic"), (00963,"A320","ST15A","Bussness"), (03214,"A320","ST21C","Bussness"), (00145,"A320","ST16A","Bussness"), (07845,"A320","ST12B","Bussness"), (03214,"B737","ST55C","economic"), 
+ (05236,"B737","ST35D","economic"), (00259,"B737","ST27A","economic"), (00236,"Max10","ST26D","economic"), (74589,"Max10","ST05E","economic"), (41256,"Max10","ST18C","economic");
+INSERT INTO ticket (booking_ref, amount) VALUES("E125",85), ("E95",90), ("E45",75),
+("E36",125), ("B06",885), ("B03",950), ("B16",980),
+("B09",1150), ("B12",1050), ("E124",150);
