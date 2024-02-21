737d97a08514dec319080598c80f9649ec670e0e
Alex <116267528+NCIAlexGar@users.noreply.github.com>
2024-02-21 21:18:24 +0000

    Add files via upload

---
+++ destination.sql
@@ -0,0 +1,2 @@
+SELECT * FROM airline.seat;INSERT INTO destination(airport_code, airport_name, city, timezone,lat,lng) VALUES("LON", "LUTON", "LONDON",'12-10-25 12:32:10', 40.589, 3.452 ), ("BUH", "Otopeni", "Bucharest", '12-10-25 12:32:10', 39.536, 2.897),
+ ("DTT", "Detroit Metropolitan Area", "Detroit", '12-10-25 12:32:10', 51.021, 4.236), ("IST", "Istanbul", "Izmir Metropolitan Area", '12-10-25 12:32:10', 58.326, 5.321), ("JKT", "Jakarta", "Jakarta Metropolitan Area", '12-10-25 12:32:10', 52.147, 4.023);
