18957a495fdc23e292729746c8e442e147e12148
Alex <116267528+NCIAlexGar@users.noreply.github.com>
2024-02-21 21:18:53 +0000

    Add files via upload

---
+++ ticket.sql
@@ -0,0 +1,3 @@
+SELECT * FROM airline.ticket;INSERT INTO ticket (booking_ref, amount) VALUES("E125",85), ("E95",90), ("E45",75),
+("E36",125), ("B06",885), ("B03",950), ("B16",980),
+("B09",1150), ("B12",1050), ("E124",150);
